4e67230b118eae42e5cfec3a837074342da52da6
Sibyl <sibyl@wormbase.org>
2018-04-11 18:12:55 -0400

    add GO domain to slim config

--- src/data/pombe.js
+++ src/data/pombe.js
@@ -2,6 +2,7 @@ export default [
   {
   "goid": "GO:0140053",
   "golabel": "mitochondrial gene expression",
+  "domain": "biological process"
   },
   {
   "goid": "GO:0007031",
@@ -10,209 +11,261 @@ export default [
   {
   "goid": "GO:0030437",
   "golabel": "ascospore formation",
+  "domain": "biological process"
   },
   {
   "goid": "GO:0023052",
   "golabel": "signaling",
+  "domain": "biological process"
   },
   {
   "goid": "GO:0030036",
   "golabel": "actin cytoskeleton organization",
+  "domain": "biological process"
   },
   {
     "goid": "GO:0006520",
     "golabel": "amino acid metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0032200",
-    "golabel": "telomere organization"
+    "golabel": "telomere organization",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0016074",
     "golabel": "snoRNA metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0005975",
     "golabel": "carbohydrate metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0070647",
     "golabel": "protein modification",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0000070",
-    "golabel": "mitotic sister chromatid segregation"
+    "golabel": "mitotic sister chromatid segregation",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0002181",
     "golabel": "cytoplasmic translation",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0030163",
     "golabel": "protein catabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0055086",
     "golabel": "nucleoside and nucleotide metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006351",
     "golabel": "DNA-dependent transcription",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006260",
     "golabel": "DNA replication",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0071554",
     "golabel": "cell wall biogenesis",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0140013",
     "golabel": "meiotic nuclear division",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006461",
     "golabel": "protein complex assembly",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0071941",
     "golabel": "nitrogen cycle metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006355",
     "golabel": "regulation of DNA-dependent transcription",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006399",
     "golabel": "tRNA metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0042254",
     "golabel": "ribosome biogenesis",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006457",
     "golabel": "protein folding",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006486",
     "golabel": "protein glycosylation",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0016071",
     "golabel": "mRNA metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0007005",
     "golabel": "mitochondrion biogenesis",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006310",
     "golabel": "DNA recombination",
+    "domain": "biological process"
   },
   {
     "goid": "GO:1901135",
     "golabel": "carbohydrate derivative metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0000747",
     "golabel": "cell fusion",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006091",
     "golabel": "metabolic energy generation",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006914",
     "golabel": "autophagy",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0098754",
     "golabel": "detoxification",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0016192",
     "golabel": "vesicular transport",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0051186",
     "golabel": "cofactor metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0007163",
     "golabel": "cell polarity",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006605",
     "golabel": "protein targeting",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0061024",
     "golabel": "membrane biogenesis",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006629",
     "golabel": "lipid metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006281",
     "golabel": "DNA repair",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0000281",
     "golabel": "mitotic cytokinesis",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0051604",
     "golabel": "protein maturation",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0007155",
     "golabel": "cell adhesion",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006766",
     "golabel": "vitamin metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006325",
     "golabel": "chromatin modification",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0000226",
     "golabel": "microtubule cytoskeleton biogenesis",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0016073",
     "golabel": "snRNA metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006915",
     "golabel": "apoptotic cell death",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0006790",
     "golabel": "sulfur metabolism",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0055065",
     "golabel": "metal ion homeostasis",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0140056",
     "golabel": "organelle localization by membrane tethering",
+    "domain": "biological process"
   },
   {
       "goid": "GO:0055085",
       "golabel": "transmembrane transport",
+      "domain": "biological process"
   },
   {
     "goid": "GO:0006913",
     "golabel": "nucleocytoplasmic transport",
+    "domain": "biological process"
   },
   {
     "goid": "GO:0008150",
-    "golabel": "other processes"
+    "golabel": "other processes",
+    "domain": "biological process"
   },
 ];
