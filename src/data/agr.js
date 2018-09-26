5b0826567bebe5a5ee816df0eee9d9f49cffc757
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2018-09-26 16:05:57 -0700

    Manual fix of agr.js slim. This file is scheduled to be removed and slim access will be managed through the slimmer function itself, specifying the name of the slim

--- src/data/agr.js
+++ src/data/agr.js
@@ -1,194 +1,262 @@
-export default [
-  {
-    'class_id': 'GO:0003674 aspect', // used as a key to a hashmap, so can't be blank
-    'class_label': 'molecular function',
-    'separator': true,
-  },
-  {
-    'class_id': 'GO:0003824',
-    'class_label': 'catalysis',
-  },
-  {
-    'class_id': 'GO:0004872',
-    'class_label': 'receptor',
-  },
-  {
-    'class_id': 'GO:0005102',
-    'class_label': 'receptor binding',
-  },
-  {
-    'class_id': 'GO:0005215',
-    'class_label': 'transporter',
-  },
-  {
-    'class_id': 'GO:0005198',
-    'class_label': 'structural molecule',
-  },
-  {
-    'class_id': 'GO:0008092',
-    'class_label': 'cytoskeleton binding',
-  },
-  {
-    'class_id': 'GO:0003677',
-    'class_label': 'DNA binding',
-  },
-  {
-    'class_id': 'GO:0003723',
-    'class_label': 'RNA binding',
-  },
-  {
-    'class_id': 'GO:0001071',
-    'class_label': 'transcription factor',
-  },
-  {
-    'class_id': 'GO:0036094',
-    'class_label': 'small molecule binding',
-  },
-  {
-    'class_id': 'GO:0046872',
-    'class_label': 'metal ion binding',
-  },
-  {
-    'class_id': 'GO:0030246',
-    'class_label': 'carbohydrate binding',
-  },
-  {
-    'class_id': 'GO:0003674',
-    'class_label': 'other activities',
-  },
-  {
-    'class_id': 'GO:0008150 aspect', // used as a key to a hashmap, so can't be blank
-    'class_label': 'biological process',
-    'separator': true,
-  },
-  {
-    'class_id': 'GO:0008283',
-    'class_label': 'cell proliferation',
-  },
-  {
-    'class_id': 'GO:0071840',
-    'class_label': 'cellular organization/biogenesis',
-  },
-  {
-    'class_id': 'GO:0051179',
-    'class_label': 'cellular transport/localization',
-  },
-  {
-    'class_id': 'GO:0032502',
-    'class_label': 'development',
-  },
-  {
-    'class_id': 'GO:0000003',
-    'class_label': 'reproduction',
-  },
-  {
-    'class_id': 'GO:0002376',
-    'class_label': 'immune system',
-  },
-  {
-    'class_id': 'GO:0050877',
-    'class_label': 'neurological',
-  },
-  {
-    'class_id': 'GO:0050896',
-    'class_label': 'response to stimulus',
-  },
-  {
-    'class_id': 'GO:0023052',
-    'class_label': 'signaling',
-  },
-  {
-    'class_id': 'GO:0010467',
-    'class_label': 'gene expression',
-  },
-  {
-    'class_id': 'GO:0019538',
-    'class_label': 'protein metabolism',
-  },
-  {
-    'class_id': 'GO:0006259',
-    'class_label': 'DNA metabolism',
-  },
-  {
-    'class_id': 'GO:0044281',
-    'class_label': 'small molecule metabolism',
-  },
-  {
-    'class_id': 'GO:0050789',
-    'class_label': 'regulation',
-  },
+export default
+[
 
-  {
-    'class_id': 'GO:0042592',
-    'class_label': 'homeostasis',
-  },
-  {
-    'class_id': 'GO:0007610',
-    'class_label': 'behavior',
-  },
-  {
-    'class_id': 'GO:0008150',
-    'class_label': 'other processes',
-  },
-  {
-    'class_id': 'GO:0005575 aspect', // used as a key to a hashmap, so can't be blank
-    'class_label': 'cellular component',
-    'separator': true,
-  },
-  {
-    'class_id': 'GO:0005576',
-    'class_label': 'extracellular',
-  },
-  {
-    'class_id': 'GO:0005737',
-    'class_label': 'cytoplasm',
-  },
-  {
-    'class_id': 'GO:0005856',
-    'class_label': 'cytoskeleton',
-  },
-  {
-    'class_id': 'GO:0005739',
-    'class_label': 'mitochondrion',
-  },
-  {
-    'class_id': 'GO:0005634',
-    'class_label': 'nucleus',
-  },
-  {
-    'class_id': 'GO:0005694',
-    'class_label': 'chromosome',
-  },
-  {
-    'class_id': 'GO:0016020',
-    'class_label': 'membrane',
-  },
-  {
-    'class_id': 'GO:0031982',
-    'class_label': 'vesicle',
-  },
-  {
-    'class_id': 'GO:0071944',
-    'class_label': 'cell periphery',
-  },
-  {
-    'class_id': 'GO:0030054',
-    'class_label': 'cell junction',
-  },
-  {
-    'class_id': 'GO:0042995',
-    'class_label': 'cell projection',
-  },
-  {
-    'class_id': 'GO:0032991',
-    'class_label': 'macromolecular complex',
-  },
-  {
-    'class_id': 'GO:0045202',
-    'class_label':  'synapse',
-  },
-  {
-    'class_id': 'GO:0005575',
-    'class_label': 'other locations',
-  },
+    {
+        "class_id": "GO:0003674 aspect", // used as a key to a hashmap, so can't be blank
+        "class_label": "molecular function",
+        "separator": true
+    },
+    {
+        "class_id": "GO:0030246",
+        "class_label": "carbohydrate binding"
+    },
+    {
+        "class_id": "GO:0097367",
+        "class_label": "carbohydrate derivative binding"
+    },
+    {
+        "class_id": "GO:0008092",
+        "class_label": "cytoskeletal protein binding"
+    },
+    {
+        "class_id": "GO:0003677",
+        "class_label": "DNA binding"
+    },
+    {
+        "class_id": "GO:0003700",
+        "class_label": "DNA-binding transcription factor activity"
+    },
+    {
+        "class_id": "GO:0030234",
+        "class_label": "enzyme regulator activity"
+    },
+    {
+        "class_id": "GO:0016787",
+        "class_label": "hydrolase activity"
+    },
+    {
+        "class_id": "GO:0016874",
+        "class_label": "ligase activity"
+    },
+    {
+        "class_id": "GO:0008289",
+        "class_label": "lipid binding"
+    },
+    {
+        "class_id": "GO:0046872",
+        "class_label": "metal ion binding"
+    },
+    // term obsoleted, see https://agr-jira.atlassian.net/browse/AGR-1163
+    // {
+    //   "class_id": "GO:0000988",
+    //   "class_label": "obsolete transcription factor activity, protein binding"
+    // },
+    {
+        "class_id": "GO:0016491",
+        "class_label": "oxidoreductase activity"
+    },
+    {
+        "class_id": "GO:0003723",
+        "class_label": "RNA binding"
+    },
+    {
+        "class_id": "GO:0038023",
+        "class_label": "signaling receptor activity"
+    },
+    {
+        "class_id": "GO:0005102",
+        "class_label": "signaling receptor binding"
+    },
+    {
+        "class_id": "GO:0036094",
+        "class_label": "small molecule binding"
+    },
+    {
+        "class_id": "GO:0005198",
+        "class_label": "structural molecule activity"
+    },
+    {
+        "class_id": "GO:0008134",
+        "class_label": "transcription factor binding"
+    },
+    {
+        "class_id": "GO:0016740",
+        "class_label": "transferase activity"
+    },
+    {
+        "class_id": "GO:0005215",
+        "class_label": "transporter activity"
+    },
+
+
+
+    {
+        "class_id": "GO:0008150 aspect", // used as a key to a hashmap, so can't be blank
+        "class_label": "biological process",
+        "separator": true
+    },
+    {
+        "class_id": "GO:0007610",
+        "class_label": "behavior"
+    },
+    {
+        "class_id": "GO:1901135",
+        "class_label": "carbohydrate derivative metabolic process"
+    },
+    {
+        "class_id": "GO:0005975",
+        "class_label": "carbohydrate metabolic process"
+    },
+    {
+        "class_id": "GO:0009056",
+        "class_label": "catabolic process"
+    },
+    {
+        "class_id": "GO:0007049",
+        "class_label": "cell cycle"
+    },
+    {
+        "class_id": "GO:0008219",
+        "class_label": "cell death"
+    },
+    {
+        "class_id": "GO:0030154",
+        "class_label": "cell differentiation"
+    },
+    {
+        "class_id": "GO:0008283",
+        "class_label": "cell proliferation"
+    },
+    {
+        "class_id": "GO:0016043",
+        "class_label": "cellular component organization"
+    },
+    {
+        "class_id": "GO:0032502",
+        "class_label": "developmental process"
+    },
+    {
+        "class_id": "GO:0006259",
+        "class_label": "DNA metabolic process"
+    },
+    {
+        "class_id": "GO:0051234",
+        "class_label": "establishment of localization"
+    },
+    {
+        "class_id": "GO:0042592",
+        "class_label": "homeostatic process"
+    },
+    {
+        "class_id": "GO:0002376",
+        "class_label": "immune system process"
+    },
+    {
+        "class_id": "GO:0006629",
+        "class_label": "lipid metabolic process"
+    },
+    {
+        "class_id": "GO:0050877",
+        "class_label": "nervous system process"
+    },
+    {
+        "class_id": "GO:0019538",
+        "class_label": "protein metabolic process"
+    },
+    {
+        "class_id": "GO:0050789",
+        "class_label": "regulation of biological process"
+    },
+    {
+        "class_id": "GO:0065009",
+        "class_label": "regulation of molecular function"
+    },
+    {
+        "class_id": "GO:0000003",
+        "class_label": "reproduction"
+    },
+    {
+        "class_id": "GO:0016070",
+        "class_label": "RNA metabolic process"
+    },
+    {
+        "class_id": "GO:0050896",
+        "class_label": "response to stimulus"
+    },
+    {
+        "class_id": "GO:0023052",
+        "class_label": "signaling"
+    },
+
+
+    {
+        "class_id": "GO:0005575 aspect", // used as a key to a hashmap, so can't be blank
+        "class_label": "cellular component",
+        "separator": true
+    },
+    {
+        "class_id": "GO:0030054",
+        "class_label": "cell junction"
+    },
+    {
+        "class_id": "GO:0042995",
+        "class_label": "cell projection"
+    },
+    {
+        "class_id": "GO:0005694",
+        "class_label": "chromosome"
+    },
+    {
+        "class_id": "GO:0031410",
+        "class_label": "cytoplasmic vesicle"
+    },
+    {
+        "class_id": "GO:0005856",
+        "class_label": "cytoskeleton"
+    },
+    {
+        "class_id": "GO:0005829",
+        "class_label": "cytosol"
+    },
+    {
+        "class_id": "GO:0005783",
+        "class_label": "endoplasmic reticulum"
+    },
+    {
+        "class_id": "GO:0005768",
+        "class_label": "endosome"
+    },
+    {
+        "class_id": "GO:0005576",
+        "class_label": "extracellular region"
+    },
+    {
+        "class_id": "GO:0005794",
+        "class_label": "golgi apparatus"
+    },
+    {
+        "class_id": "GO:0005739",
+        "class_label": "mitochondrion"
+    },
+    {
+        "class_id": "GO:0005634",
+        "class_label": "nucleus"
+    },
+    {
+        "class_id": "GO:0005886",
+        "class_label": "plasma membrane"
+    },
+    {
+        "class_id": "GO:0032991",
+        "class_label": "protein-containing complex"
+    },
+    {
+        "class_id": "GO:0045202",
+        "class_label": "synapse"
+    },
+    {
+        "class_id": "GO:0005773",
+        "class_label": "vacuole"
+    }
+
 ];
