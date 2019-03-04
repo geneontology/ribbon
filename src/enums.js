fe0d4af8c3a83c992bd214a4f90faa4c63bdaf90
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2019-03-04 03:16:01 -0800

    Better customization of the table + new SASS vars + add label position bottom

--- src/enums.js
+++ src/enums.js
@@ -3,7 +3,7 @@
 var COLOR_BY = { "CLASS_COUNT": 0, "ANNOTATION_COUNT": 1 }
 
 // Enum to control the label position of each entity
-var POSITION = { "NONE" : 0, "LEFT" : 1, "RIGHT" : 2 }
+var POSITION = { "NONE" : 0, "LEFT" : 1, "RIGHT" : 2, "BOTTOM": 3 }
 
 
 exports.COLOR_BY = COLOR_BY;
