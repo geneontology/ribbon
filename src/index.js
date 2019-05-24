406bbdb960664065ae9feb8902beb58d5c19ada5
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2019-05-24 12:21:02 -0700

    updating export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Ribbon from './Ribbon';
 import RibbonDataProvider from './RibbonDataProvider';
 import RibbonBase from './RibbonBase';
+import GenericRibbon from './components/GenericRibbon';
 
 export default Ribbon;
 
 export {
   RibbonDataProvider,
   RibbonBase,
+  GenericRibbon
 };
